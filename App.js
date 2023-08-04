ef0d33b1ab3740d0452dcbc725a07871aa334186
akshayjoshi <joshiakshay000@gmail.com>
2023-08-04 13:10:43 +0530

    carousel update

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import GetImages from './screens/GetImages';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <GetImages />
       <StatusBar style="auto" />
     </View>
@@ -16,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#323035',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- screens/GetImages.js
+++ screens/GetImages.js
@@ -1,33 +1,63 @@
-import React from 'react';
-import database from '../firebase/config'; // Adjust the path as needed
+import React, { useState, useEffect } from 'react';
+import database from '../firebase/config';
 import { ref, get } from 'firebase/database';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const documentId = '-NahQvFwvx4LVvwSmC65';
 
-export const getImages = async () => {
-  try {
-    const imagesUrlRef = ref(database, 'restaurants/' + 'undefined/'+ documentId  + '/imagesBucketURL');
-    const imagesUrlsSnapshot = await get(imagesUrlRef);
-    console.log("imagesUrlRef url's :", imagesUrlRef);
-    const imagesUrls = imagesUrlsSnapshot.val();
-    
-    console.log("image url's :", imagesUrls);
-    return imagesUrls;
-  } catch (error) {
-    console.error("Error fetching images URLs:", error);
-    return null;
-  }
-};
-
 const GetImages = () => {
-  const handleButtonClick = async () => {
-    await getImages();
-  };
+  const [imagesUrls, setImagesUrls] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const imagesUrlRef = ref(database, 'restaurants/' + 'undefined/' + documentId + '/imagesBucketURL');
+        const imagesUrlsSnapshot = await get(imagesUrlRef);
+        const imagesUrls = imagesUrlsSnapshot.val();
+        console.log("image url array:", imagesUrls);
+        setImagesUrls(imagesUrls);
+      } catch (error) {
+        console.error("Error fetching images URLs:", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
 
   return (
-    <div>
-      <h1>This is GetImages page</h1>
-      <button onClick={handleButtonClick}>Get Images</button>
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
+      {imagesUrls.length > 0 && (
+        <div style={{ height: '80vh', width: '100%', position: 'relative', marginTop:'15vh' }}>
+          <Carousel
+            zoom={true}
+            showArrows={false}
+            showStatus={false}
+            showIndicators={false}
+            showBullets={true}
+            showThumbs={false}
+            emulateTouch
+            infiniteLoop={false}
+            autoPlay={false}
+            interval={3000}
+            transitionTime={500}
+            lazyLoad={true}
+            showNav={true}
+            showThumbnails={true}
+          >
+            {imagesUrls.map((imageUrl, index) => (
+              <div key={index} style={{ height: '100%', width: '100%' }}>
+                <img
+                  src={imageUrl}
+                  alt={`Image ${index}`}
+                  style={{ objectFit: 'cover', height: '100%', width: '100%' }}
+                />
+              </div>
+            ))}
+          </Carousel>
+        </div>
+      )}
+      <h5>*prices may vary, Not handled by restauraunt</h5>
     </div>
   );
 };
